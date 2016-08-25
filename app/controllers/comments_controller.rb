20acecb0896358590cf1f07f7b3d9c4099dbb87f
Bryan Anderson <bryannobleanderson@live.com>
2016-08-25 16:06:46 -0600

    added stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,54 @@
 class CommentsController < ApplicationController
+  before_action :comment, except: [:index, :new, :create]
+  before_action :post, only: [:show]
+
   def index
+    @comments = @post.comments
+  end
+
+  def new
+    @comment = comment.new
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
-  def new
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+    redirect_to post_comment_path(@post)
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description, :category, :location, :completed)
+  end
+
+  def comment
+    @comment = comment.find(params[:id])
+  end
+
+  def post
+    @post = post.find(params[:post_id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,49 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :post, except: [:index, :new, :create]
 
   def index
     @posts = Post.all
   end
 
-  def show
-
+  def new
+    @post = Post.new
   end
 
-  def new
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
-  def create
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   def destroy
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
 
   def post_params
+    params.require(:post).permit(:title, :user_id)
   end
 
   def post
+    @post = Post.find(params[:id])
   end
 
 end
