0b851d3d25cb8e67f5a85114de8525948f37b549
Bryan Anderson <bryannobleanderson@live.com>
2016-08-26 12:06:47 -0600

    added more stuffs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :post, except: [:index, :new, :create]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all.paginate(page: params[:page], per_page: 2)
+    @posts = Post.all.paginate(page: params[:page], per_page: 8).order(created_at: :desc)
   end
 
   def new
@@ -44,6 +45,11 @@ class PostsController < ApplicationController
 
   def post
     @post = Post.find(params[:id])
+  end
+
+  def authorized_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to posts_path, notice: "You are not authorized, son" if @post.nil?
   end
 
 end
